3b8fa93c739105b2c81ff3f58d5f48a1647c3f14
mamingzhang <mamingzhang@shuzijiayuan.com>
2017-08-25 16:53:45 +0800

    Add ndk-gdb Test

--- app/src/main/java/com/horsege/ndkmultidirtest/MainActivity.java
+++ app/src/main/java/com/horsege/ndkmultidirtest/MainActivity.java
@@ -2,9 +2,10 @@ package com.horsege.ndkmultidirtest;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +14,15 @@ public class MainActivity extends AppCompatActivity {
 
         ((TextView)findViewById(R.id.model1)).setText(new Model1().getModelName());
         ((TextView)findViewById(R.id.model2)).setText(new Model2().getModelName());
+
+        findViewById(R.id.model3).setVisibility(View.GONE);
+
+        findViewById(R.id.opBtn).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        findViewById(R.id.model3).setVisibility(View.VISIBLE);
         ((TextView)findViewById(R.id.model3)).setText(new Model3().getModelName());
     }
 }
